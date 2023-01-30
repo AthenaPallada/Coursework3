66302da2477df4807ac830047618573b38ac5851
tatiana_alekseeva <tatianaalekseeva1313@gmail.com>
2023-01-30 19:16:21 +0300

    Coursework3

---
+++ src/main/java/tatiana/me/springweb/coursework3/Coursework3Application.java
@@ -0,0 +1,15 @@
+package tatiana.me.springweb.coursework3;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition
+@SpringBootApplication
+public class Coursework3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework3Application.class, args);
+    }
+
+}
